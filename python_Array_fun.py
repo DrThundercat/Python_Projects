60e5a4c1ffd7e8a464a1c585ea8ce8c06dee321f
Jamie White <jamie.parris.white@gmail.com>
2017-09-16 14:45:08 -0400

    adding python_Array_fun

---
+++ python_Array_fun.py
@@ -0,0 +1,10 @@
+#variables
+ingredients = ['onions','tomato','orange','grape','apple','banana','chicken','duck','thing']
+count = 1
+
+#print ingreadents and order
+
+for i in ingredients:
+    x = str(count) +" " + i
+    count = count +1
+    print (x) 
